d73fa45745b78c4487730923fb05f999b09020b1
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2025-10-30 12:24:19 -0400

    Add prisma, connect to psql db

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require("path");
 require("dotenv").config();
 
+const indexRouter = require("./routes/indexRouter");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", indexRouter);
 
 // development
 const PORT = process.env.PORT || 3000;
---
+++ prisma.config.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+// import path from "node:path";
+import { defineConfig, env } from "prisma/config";
+
+// type Env = {
+//   DATABASE_URL: string;
+// };
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
