e8284bb0287f96624775f110a3eabb6ec9c6b369
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2025-10-31 10:05:23 -0400

    Add prisma session store. Update schema

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 require("dotenv").config();
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+// const { PrismaClient } = require("./generated/prisma");
+const prisma = new PrismaClient();
 
 const indexRouter = require("./routes/indexRouter");
 
@@ -9,6 +14,22 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 app.use("/", indexRouter);
 
